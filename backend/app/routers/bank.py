170eaa7011a3a80d40be7bf4ef061a0555cef48b
Potassiumxx <brainiacgamer444@gmail.com>
2025-10-29 21:48:54 +0545

    feat: add condition to prevent similar bank's name being created twice
    Check if the bank's name already exists before adding it in the
    database, and if it does, then send a "400 Bad Request" message.

--- backend/app/routers/bank.py
+++ backend/app/routers/bank.py
@@ -1,16 +1,23 @@
-from fastapi import Depends
+from fastapi import Depends, HTTPException
 from fastapi.routing import APIRouter
 from sqlalchemy.orm import Session
 
 from app.db import get_db
 from app.models.bank import Bank
+from app.schemas.bank import BankCreate, BankResponse
 
-router = APIRouter()
+router = APIRouter(prefix="/banks", tags=["Banks"])
 
 
-@router.post("/create-bank")
-def create_bank(name: str, db: Session = Depends(get_db)):
-	bank = Bank(name=name)
+@router.post("/", response_model=BankResponse)
+def create_bank(value: BankCreate, db: Session = Depends(get_db)):
+	existing = db.query(Bank).filter(Bank.name == value.name).first()
+	if existing:
+		raise HTTPException(
+			status_code=400, detail=f"'{value.name}' bank already exists"
+		)
+
+	bank = Bank(name=value.name)
 	db.add(bank)
 	db.commit()
 	db.refresh(bank)
