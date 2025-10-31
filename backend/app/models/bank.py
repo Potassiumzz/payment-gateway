d0eaf3ded67fd58089244d10c63d35c63e3208aa
Potassiumxx <brainiacgamer444@gmail.com>
2025-10-31 20:09:20 +0545

    refactor: use Mapped classes instead of Columns for database tables
    - Mapped classes is apparently the new and preferred syntax which is
      type-checker friendly. Without this new syntax, the Pyright would
      throw some type error when trying to update a value in the database
      table.
    
      See next commit's message to get better idea about this.

--- backend/app/models/bank.py
+++ backend/app/models/bank.py
@@ -1,5 +1,7 @@
-from sqlalchemy import Column, ForeignKey, Integer, Numeric, String
-from sqlalchemy.orm import relationship
+from decimal import Decimal
+
+from sqlalchemy import ForeignKey, Integer, Numeric, String
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from app.db import Base
 from app.globals.enums import TableName
@@ -8,8 +10,8 @@ from app.globals.enums import TableName
 class Bank(Base):
 	__tablename__ = TableName.BANKS.value
 
-	id = Column(Integer, primary_key=True, index=True)
-	name = Column(String, unique=True, index=True, nullable=False)
+	id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+	name: Mapped[str] = mapped_column(String, unique=True, index=True, nullable=False)
 
 	accounts = relationship("BankAccount", back_populates="bank")
 
@@ -17,11 +19,17 @@ class Bank(Base):
 class BankAccount(Base):
 	__tablename__ = TableName.BANK_ACCOUNTS.value
 
-	id = Column(Integer, primary_key=True, index=True)
-	account_number = Column(Integer, unique=True, index=True, nullable=False)
-	balance = Column(Numeric(10, 2), nullable=False)  # max value = 99999999.99
-	owner_name = Column(String, nullable=False)
+	id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+	account_number: Mapped[int] = mapped_column(
+		Integer, unique=True, index=True, nullable=False
+	)
+	balance: Mapped[Decimal] = mapped_column(
+		Numeric(10, 2), nullable=False
+	)  # max value = 99999999.99
+	owner_name: Mapped[str] = mapped_column(String, nullable=False)
 
-	bank_id = Column(Integer, ForeignKey("banks.id"), nullable=False)
+	bank_id: Mapped[int] = mapped_column(
+		Integer, ForeignKey("banks.id"), nullable=False
+	)
 
 	bank = relationship("Bank", back_populates="accounts")
