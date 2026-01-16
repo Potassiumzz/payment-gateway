ea828b4ff368dd6c78420d2bc1a96d38ce45733b
Potassiumxx <brainiacgamer444@gmail.com>
2026-01-16 15:35:35 +0545

    allow CORS from the backend
    - Created a `template.env` file as well.

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,16 @@
+import os
+
+from dotenv import load_dotenv
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 # from app.db import Base, engine
 from app.routers import account_pin, bank, bank_account, payment_intent, transaction
 
 app = FastAPI()
+load_dotenv()
+
+frontend_url = os.getenv("FRONTEND_URL", "").split(",")
 
 # Create tables
 # Base.metadata.create_all(bind=engine)
@@ -13,6 +20,16 @@ app.include_router(bank_account.router)
 app.include_router(transaction.router)
 app.include_router(account_pin.router)
 app.include_router(payment_intent.router)
+
+print("frontend: ", frontend_url)
+
+app.add_middleware(
+	CORSMiddleware,
+	allow_origins=frontend_url,
+	allow_credentials=True,
+	allow_methods=["*"],
+	allow_headers=["*"],
+)
 
 
 @app.get("/")
