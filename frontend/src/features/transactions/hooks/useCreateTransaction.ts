110003055089717ed1218455355619d4783b4ae4
Potassiumzz <potassiumzxz@gmail.com>
2026-04-17 22:56:39 +0545

    feat: add an idempotency key while creating a transaction
    - Create an idempotency key while creating a transaction to prevent
      duplicate payment.

--- frontend/src/features/transactions/hooks/useCreateTransaction.ts
+++ frontend/src/features/transactions/hooks/useCreateTransaction.ts
@@ -4,6 +4,7 @@ import {
   TransactionResponse,
 } from "@/features/transactions/types/transaction";
 import { TRANSACTIONS_ENDPOINT } from "@/constants/endpoints";
+import { LOCAL_STORAGE_KEYS } from "@/constants/keys";
 
 export function useCreateTransaction() {
   const { mutate, error, isLoading } = useMutation<
@@ -11,12 +12,26 @@ export function useCreateTransaction() {
     TransactionResponse
   >();
 
-  const createTransactionRequestHeader = {
-    "Idempotency-Key": crypto.randomUUID(),
-  };
-
   return {
     createTransaction: (data: CreateTransactionPayload) => {
+      const createTransactionRequestHeader = {
+        "Idempotency-Key": "",
+      };
+
+      const idempotencyKey = localStorage.getItem(
+        LOCAL_STORAGE_KEYS.idempotencyKey,
+      );
+
+      if (idempotencyKey) {
+        createTransactionRequestHeader["Idempotency-Key"] = idempotencyKey;
+      } else {
+        createTransactionRequestHeader["Idempotency-Key"] = crypto.randomUUID();
+        localStorage.setItem(
+          LOCAL_STORAGE_KEYS.idempotencyKey,
+          `${createTransactionRequestHeader["Idempotency-Key"]}`,
+        );
+      }
+
       mutate({
         url: TRANSACTIONS_ENDPOINT,
         input: data,
