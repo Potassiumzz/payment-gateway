a9a6924e642ba68b586fef7756c27e4df8d040b9
Potassiumzz <potassiumzxz@gmail.com>
2026-05-23 13:34:23 +0545

    feat: add 401, and 403 error raising functions

--- backend/app/utils/utils.py
+++ backend/app/utils/utils.py
@@ -9,9 +9,17 @@ def generate_intent_id() -> str:
 	return f"k_{uuid.uuid4().hex}"
 
 
+def raise_400_error():
+	raise HTTPException(status_code=400, detail=ResponseError.BAD_REQUEST.value)
+
+
+def raise_401_error():
+	raise HTTPException(status_code=401, detail=ResponseError.UNAUTHORISED.value)
+
+
+def raise_403_error(message: str):
+	raise HTTPException(status_code=403, detail=message)
+
+
 def raise_404_error():
 	raise HTTPException(status_code=404, detail=ResponseError.RESOURCE_NOT_FOUND.value)
-
-
-def raise_400_error():
-	raise HTTPException(status_code=400, detail=ResponseError.RESOURCE_EXISTS.value)
