32d9f1b3afc21eb327c205b73f7bead631c04533
Potassiumzz <potassiumzxz@gmail.com>
2026-05-23 14:42:30 +0545

    refact: add response type and description in router's method parentheses
    - Only for bank account route only yet.

--- backend/app/routers/bank_account.py
+++ backend/app/routers/bank_account.py
@@ -3,7 +3,6 @@ from fastapi.routing import APIRouter
 
 from app.dependencies.bank_account import BankAccountDependencies
 from app.globals.enums import RouterPrefix, RouterTag
-from app.models import BankAccount
 from app.schemas.bank_account import AccountCreate, AccountResponse, AccountUpdate
 from app.services.bank_account import BankAccountService
 
@@ -16,28 +15,33 @@ router = APIRouter(prefix=RouterPrefix.ACCOUNTS.value, tags=[RouterTag.ACCOUNTS.
 def create_account(
 	value: AccountCreate,
 	service: BankAccountService = Depends(BankAccountDependencies.get_service),
-) -> AccountResponse:
+):
 	return service.create(value)
 
 
-@router.put("/{id}")
+@router.put(
+	"/{id}",
+	response_model=AccountResponse,
+	description="Update the bank account by its ID. Only the account's name and PIN can be updated.",
+)
 def update_account(
 	id: int,
 	account: AccountUpdate,
 	service: BankAccountService = Depends(BankAccountDependencies.get_service),
-) -> AccountResponse:
+):
 	return service.update(id, account)
 
 
 @router.delete(
 	"/{id}",
 	status_code=204,
+	response_model=None,
 	description="Soft delete the account by making the account's status inactive, rather than deleting the account from database.",
 )
 def delete_account(
 	id: int,
 	service: BankAccountService = Depends(BankAccountDependencies.get_service),
-) -> None:
+):
 	return service.delete(id)
 
 
@@ -49,19 +53,27 @@ def delete_account(
 def hard_delete(
 	id: int,
 	service: BankAccountService = Depends(BankAccountDependencies.get_service),
-) -> None:
+):
 	return service.hard_delete(id)
 
 
-@router.get("/")
+@router.get(
+	"/",
+	response_model=list[AccountResponse],
+	description="Get the list of all bank accounts.",
+)
 def get_accounts_list(
 	service: BankAccountService = Depends(BankAccountDependencies.get_service),
-) -> list[AccountResponse]:
+):
 	return service.get_all()
 
 
-@router.get("/{id}")
+@router.get(
+	"/{id}",
+	response_model=AccountResponse,
+	description="Get the bank account by its ID.",
+)
 def get_account(
 	id: int, service: BankAccountService = Depends(BankAccountDependencies.get_service)
-) -> AccountResponse:
+):
 	return service.get_by_id(id)
