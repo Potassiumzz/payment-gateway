5746fcc6a714178df92319279848f065c3077ccb
Potassiumzz <potassiumzxz@gmail.com>
2026-06-15 19:03:57 +0545

    feat: return error status from `useGetTransactionByID` hook

--- frontend/src/features/transactions/hooks/useGetTransactionByID.ts
+++ frontend/src/features/transactions/hooks/useGetTransactionByID.ts
@@ -3,11 +3,11 @@ import type { TransactionResponse } from "../types/transaction";
 import { BACKEND_ENDPOINTS } from "@/constants/endpoints";
 
 export function useGetTransactionByID(transactionId: string) {
-	const { data, error, isLoading } = useQuery<TransactionResponse>({
+	const { data, error, errorStatus, isLoading } = useQuery<TransactionResponse>({
 		url: BACKEND_ENDPOINTS.TRANSACTIONS_ENDPOINT,
 		queryKey: "transaction_detail",
 		id: transactionId,
 	});
 
-	return { transactionDetail: data, error, isLoading };
+	return { transactionDetail: data, error, errorStatus, isLoading };
 }
