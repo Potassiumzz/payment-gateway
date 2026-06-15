c813dbb04824b9c7077157d946fbc238f6609ac3
Potassiumzz <potassiumzxz@gmail.com>
2026-06-15 16:11:51 +0545

    feat: create transaction status constant according to backend

--- frontend/src/features/transactions/types/transaction.ts
+++ frontend/src/features/transactions/types/transaction.ts
@@ -12,8 +12,15 @@ export interface TransactionResponse {
 	receiver_account_number: number;
 	receiver_owner_name: string;
 	receiver_bank_name: string;
-	status: string;
+	status: TransactionStatusType;
 	failure_reason: string;
 	amount_transferred: string;
 	timestamp: Date;
 }
+
+export const TransactionStatus = {
+	Successful: "Successful",
+	Failure: "Failure",
+} as const;
+
+export type TransactionStatusType = (typeof TransactionStatus)[keyof typeof TransactionStatus];
