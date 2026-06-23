e96e954291d3df399f9b572fdd1c5b58c16f0f41
Potassiumzz <potassiumzxz@gmail.com>
2026-06-23 19:37:34 +0545

    fix: create a type for account endpoint for query and path segment
    - The account endpoint needs both query and path segments to query and
      delete. So the `Endpoint` type was updated to take that in account so
      that we do not get any type errors.

--- frontend/src/api/config/types.ts
+++ frontend/src/api/config/types.ts
@@ -1,9 +1,11 @@
 import type { BACKEND_ENDPOINTS } from "@/constants/endpoints";
 
 export type APIMethods = "GET" | "POST" | "PUT" | "DELETE";
-export type Endpoint =
-	| (typeof BACKEND_ENDPOINTS)[keyof typeof BACKEND_ENDPOINTS]
-	| `${typeof BACKEND_ENDPOINTS.ACCOUNT_ENDPOINT}?${string}`;
+export type Endpoint = (typeof BACKEND_ENDPOINTS)[keyof typeof BACKEND_ENDPOINTS] | AccountEndpoint;
+
+type AccountEndpoint =
+	| `${typeof BACKEND_ENDPOINTS.ACCOUNT_ENDPOINT}?${string}`
+	| `${typeof BACKEND_ENDPOINTS.ACCOUNT_ENDPOINT}${number}`;
 
 export interface IAPI<T> {
 	method: APIMethods;
