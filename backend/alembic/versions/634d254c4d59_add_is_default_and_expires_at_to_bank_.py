fce571ae373584057561ab30b70a1e2543a34acb
Potassiumzz <potassiumzxz@gmail.com>
2026-06-27 00:04:03 +0545

    refact: alembic revision file

--- backend/alembic/versions/634d254c4d59_add_is_default_and_expires_at_to_bank_.py
+++ backend/alembic/versions/634d254c4d59_add_is_default_and_expires_at_to_bank_.py
@@ -8,9 +8,9 @@ Create Date: 2026-06-23 17:55:50.343764
 
 from typing import Sequence, Union
 
-from alembic import op
 import sqlalchemy as sa
 
+from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = "634d254c4d59"
