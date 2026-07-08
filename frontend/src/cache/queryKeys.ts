79536fa67abbec3573728cbb9fb125ced9c010ab
Eightball <eightball.code@gmail.com>
2026-07-08 19:58:26 +0545

    fix: store the cache value for account based on page number, or search
    - The goal is to create a separate key for cache depending on page
      number, or the searched keywords.

--- frontend/src/cache/queryKeys.ts
+++ frontend/src/cache/queryKeys.ts
@@ -1,5 +1,7 @@
+const ACCOUNT_LIST = "ACCOUNT_LIST";
+
 export const QUERY_KEYS = {
 	INTENT_DETAIL: "INTENT_DETAIL",
 	TRANSACTION_DETAIL: "TRANSACTION_DETAIL",
-	ACCOUNT_LIST: "ACCOUNT_LIST",
+	ACCOUNT_LIST: (page: number, search: string) => `${ACCOUNT_LIST}_${page}_${search}`,
 } as const;
