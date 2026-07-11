7342df51e31e0e581a8cc8a7bec6cbfe78378460
Eightball <eightball.code@gmail.com>
2026-07-11 10:38:53 +0545

    feat: create type of payment intent status

--- frontend/src/features/payments/types/paymentIntent.ts
+++ frontend/src/features/payments/types/paymentIntent.ts
@@ -7,8 +7,17 @@ export interface PaymentIntentPayload {
 export interface PaymentIntentResponse {
 	id: string;
 	amount: string;
-	status: string;
+	status: PaymentIntentStatusType;
 	return_url?: string;
 	receiver_account_number?: number;
 	checkout_url: string;
 }
+
+export const PaymentIntentStatus = {
+	REQUIRES_PAYMENT: "RequiresPayment",
+	PENDING: "Pending",
+	SUCCEEDED: "Succeeded",
+	FAILED: "Failed",
+} as const;
+
+export type PaymentIntentStatusType = (typeof PaymentIntentStatus)[keyof typeof PaymentIntentStatus];
