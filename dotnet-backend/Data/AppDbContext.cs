9ebe3cd092c5d4c5a3c29263214aac14ac1377f9
Eightball <eightball.code@gmail.com>
2026-07-28 17:03:26 +0545

    fix: block deleting a bank which has FK reference to accounts
    - By default, the EF Core has `Cascade` behaviour for deletion, which is
      restricted in case of Bank's deletion.

--- dotnet-backend/Data/AppDbContext.cs
+++ dotnet-backend/Data/AppDbContext.cs
@@ -7,6 +7,15 @@ public class AppDbContext : DbContext
 {
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<BankAccount>()
+            .HasOne(ba => ba.Bank)
+            .WithMany()
+            .HasForeignKey(ba => ba.BankId)
+            .OnDelete(DeleteBehavior.Restrict);
+    }
+
     public DbSet<Bank> Banks { get; set; } = null!;
     public DbSet<BankAccount> BankAccounts { get; set; } = null!;
 }
