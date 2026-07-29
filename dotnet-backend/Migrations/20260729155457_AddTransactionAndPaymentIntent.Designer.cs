f283a04410c1962f14f81e8be0b805f1e5c9d25f
Eightball <eightball.code@gmail.com>
2026-07-29 21:46:08 +0545

    feat: run the migration for Transaction and PaymentIntent models

--- dotnet-backend/Migrations/20260729155457_AddTransactionAndPaymentIntent.Designer.cs
+++ dotnet-backend/Migrations/20260729155457_AddTransactionAndPaymentIntent.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Ntay.Data;
 
 #nullable disable
@@ -9,9 +10,11 @@ using Ntay.Data;
 namespace Ntay.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260729155457_AddTransactionAndPaymentIntent")]
+    partial class AddTransactionAndPaymentIntent
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
