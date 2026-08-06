6c1ddec8ebdbf4938e391dd169892de7f259f333
Eightball <eightball.code@gmail.com>
2026-08-06 20:26:09 +0545

    feat: create controller for BanAccount entity

---
+++ dotnet-backend/Controllers/BankAccountController.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Ntay.Dtos.BankAccount;
+using Ntay.Services;
+
+namespace Ntay.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class BankAccountController : ControllerBase
+{
+    private readonly BankAccountService _accountService;
+
+    public BankAccountController(BankAccountService accountService)
+    {
+        _accountService = accountService;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<List<BankAccountResponse>>> GetAccounts()
+    {
+        return Ok(await _accountService.GetAccountsAsync());
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<BankAccountResponse>> GetAccount(int id)
+    {
+        BankAccountResponse? account = await _accountService.GetAccountById(id);
+        return account is not null ? Ok(account) : NotFound();
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<BankAccountResponse>> CreateAccount(CreateAccountRequest request)
+    {
+        try
+        {
+            BankAccountResponse account = await _accountService.CreateAccountAsync(request);
+            return CreatedAtAction(nameof(GetAccount), new { id = account.Id }, account);
+        }
+        catch (InvalidOperationException e)
+        {
+            return BadRequest(new { error = e.Message });
+        }
+    }
+
+    [HttpPut("{id}")]
+    public async Task<ActionResult<BankAccountResponse>> UpdateAccount(
+        int id,
+        UpdateAccountRequest request
+    )
+    {
+        try
+        {
+            BankAccountResponse? account = await _accountService.UpdateAccountAsync(id, request);
+            return account is not null ? Ok(account) : NotFound();
+        }
+        catch (InvalidOperationException e)
+        {
+            return NotFound(new { error = e.Message });
+        }
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteAccount(int id)
+    {
+        bool account = await _accountService.DeactivateAccountAsync(id);
+        return account ? NoContent() : NotFound();
+    }
+}
