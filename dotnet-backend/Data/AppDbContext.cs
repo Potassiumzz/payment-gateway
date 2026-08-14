7ff65369f250c223687ac488b4d1094bcdfd6fb9
Eightball <eightball.code@gmail.com>
2026-08-14 22:04:36 +0545

    feat: make tables and columns name to snake_case for database
    - Postgres uses snake_case style for tables and columns, but C# uses
      PascalCase. Since PascalCase causes unnecessary problems with
      postgres, and since postgres just uses snake_case as default style,
      changing the PascalCase to snake_case was needed.

--- dotnet-backend/Data/AppDbContext.cs
+++ dotnet-backend/Data/AppDbContext.cs
@@ -1,5 +1,6 @@
 namespace Ntay.Data;
 
+using System.Linq;
 using Microsoft.EntityFrameworkCore;
 using Ntay.Models;
 
@@ -10,7 +11,11 @@ public class AppDbContext : DbContext
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        base.OnModelCreating(modelBuilder);
+
         modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+
+        ApplySnakeCaseNames(modelBuilder);
     }
 
     public DbSet<Bank> Banks { get; set; } = null!;
@@ -19,4 +24,44 @@ public class AppDbContext : DbContext
     public DbSet<PaymentIntent> PaymentIntents { get; set; } = null!;
     public DbSet<AccountPin> AccountPins { get; set; } = null!;
     public DbSet<IdempotencyKey> IdempotencyKeys { get; set; } = null!;
+
+    private static void ApplySnakeCaseNames(ModelBuilder modelBuilder)
+    {
+        foreach (var entity in modelBuilder.Model.GetEntityTypes())
+        {
+            entity.SetTableName(ToSnakeCase(entity.GetTableName()!));
+
+            foreach (var property in entity.GetProperties())
+            {
+                property.SetColumnName(ToSnakeCase(property.GetColumnName()));
+            }
+
+            foreach (var key in entity.GetKeys())
+            {
+                key.SetName(ToSnakeCase(key.GetName()!));
+            }
+
+            foreach (var fk in entity.GetForeignKeys())
+            {
+                fk.SetConstraintName(ToSnakeCase(fk.GetConstraintName()!));
+            }
+
+            foreach (var index in entity.GetIndexes())
+            {
+                index.SetDatabaseName(ToSnakeCase(index.GetDatabaseName()!));
+            }
+        }
+    }
+
+    private static string ToSnakeCase(string input)
+    {
+        return string.Concat(
+            input.Select(
+                (c, i) =>
+                    i > 0 && char.IsUpper(c)
+                        ? "_" + char.ToLowerInvariant(c)
+                        : char.ToLowerInvariant(c).ToString()
+            )
+        );
+    }
 }
