d658343ed28ecae4d2d1a2935085753ac657ef4b
Eightball <eightball.code@gmail.com>
2026-08-15 16:00:20 +0545

    feat: enable CORS

--- dotnet-backend/Program.cs
+++ dotnet-backend/Program.cs
@@ -7,14 +7,17 @@ using Ntay.Data;
 using Ntay.Services;
 
 var builder = WebApplication.CreateBuilder(args);
+var allowedOrigins = builder.Configuration.GetSection("Cors:AllowedOrigins").Get<string[]>() ?? [];
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
 builder.Services.AddScoped<BankService>();
 builder.Services.AddScoped<BankAccountService>();
 builder.Services.AddScoped<AccountPinService>();
@@ -22,7 +25,20 @@ builder.Services.AddScoped<PaymentIntentService>();
 builder.Services.AddScoped<IdempotencyService>();
 builder.Services.AddScoped<TransactionService>();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(
+        "Default",
+        policy =>
+        {
+            policy.WithOrigins(allowedOrigins).AllowAnyHeader().AllowAnyMethod().AllowCredentials();
+        }
+    );
+});
+
 var app = builder.Build();
+
+app.UseCors("Default");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
