77f023e0e5e7fdc7fbcc012122b70e610cc2a577
Eightball <eightball.code@gmail.com>
2026-08-16 21:12:54 +0545

    feat: create new EF Core migration and remove the old ones
    - Create a new migration with all tables and fields in snake_case,
      following the Postgresql convention.
      The old migration was in PascalCase, and `account_pins` and
      `idempotency_keys` tables were missing anyway (I had not ran
      migrations for them).

--- dotnet-backend/Migrations/20260816145739_InitialCreate.Designer.cs
+++ dotnet-backend/Migrations/20260816145739_InitialCreate.Designer.cs
@@ -2,6 +2,9 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Ntay.Data;
 
 #nullable disable
@@ -9,9 +12,11 @@ using Ntay.Data;
 namespace Ntay.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260816145739_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ dotnet-backend/Migrations/20260816145739_InitialCreate.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Ntay.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "banks",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("p_k_banks", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "transaction_response",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    payment_intent_id = table.Column<string>(type: "text", nullable: false),
+                    sender_account_number = table.Column<int>(type: "integer", nullable: false),
+                    sender_owner_name = table.Column<string>(type: "text", nullable: false),
+                    sender_bank_name = table.Column<string>(type: "text", nullable: false),
+                    receiver_account_number = table.Column<int>(type: "integer", nullable: false),
+                    receiver_owner_name = table.Column<string>(type: "text", nullable: false),
+                    receiver_bank_name = table.Column<string>(type: "text", nullable: false),
+                    status = table.Column<int>(type: "integer", nullable: false),
+                    failure_reason = table.Column<string>(type: "text", nullable: true),
+                    amount_transferred = table.Column<decimal>(type: "numeric", nullable: false),
+                    timestamp = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    return_url = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("p_k_transaction_response", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "bank_accounts",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    account_number = table.Column<int>(type: "integer", nullable: false),
+                    balance = table.Column<decimal>(type: "numeric", nullable: false),
+                    owner_name = table.Column<string>(type: "text", nullable: false),
+                    is_active = table.Column<bool>(type: "boolean", nullable: false),
+                    is_default = table.Column<bool>(type: "boolean", nullable: false),
+                    expires_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    bank_id = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("p_k_bank_accounts", x => x.id);
+                    table.ForeignKey(
+                        name: "f_k_bank_accounts_banks_bank_id",
+                        column: x => x.bank_id,
+                        principalTable: "banks",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "idempotency_keys",
+                columns: table => new
+                {
+                    key = table.Column<string>(type: "text", nullable: false),
+                    endpoint = table.Column<string>(type: "text", nullable: false),
+                    response_body_id = table.Column<int>(type: "integer", nullable: false),
+                    status = table.Column<int>(type: "integer", nullable: false),
+                    failure_reason = table.Column<string>(type: "text", nullable: true),
+                    created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("p_k_idempotency_keys", x => x.key);
+                    table.ForeignKey(
+                        name: "f_k_idempotency_keys_transaction_response_response_body_id",
+                        column: x => x.response_body_id,
+                        principalTable: "transaction_response",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "account_pins",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    pin_hash = table.Column<string>(type: "text", nullable: false),
+                    failed_attempts = table.Column<int>(type: "integer", nullable: false),
+                    locked_until = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    bank_account_id = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("p_k_account_pins", x => x.id);
+                    table.ForeignKey(
+                        name: "f_k_account_pins__bank_accounts_bank_account_id",
+                        column: x => x.bank_account_id,
+                        principalTable: "bank_accounts",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "payment_intents",
+                columns: table => new
+                {
+                    id = table.Column<string>(type: "text", nullable: false),
+                    amount = table.Column<decimal>(type: "numeric", nullable: false),
+                    status = table.Column<int>(type: "integer", nullable: false),
+                    attempt_count = table.Column<int>(type: "integer", nullable: false),
+                    return_url = table.Column<string>(type: "text", nullable: true),
+                    created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    receiver_account_id = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("p_k_payment_intents", x => x.id);
+                    table.ForeignKey(
+                        name: "f_k_payment_intents_bank_accounts_receiver_account_id",
+                        column: x => x.receiver_account_id,
+                        principalTable: "bank_accounts",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "transactions",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    amount_transferred = table.Column<decimal>(type: "numeric", nullable: false),
+                    status = table.Column<int>(type: "integer", nullable: false),
+                    failure_reason = table.Column<string>(type: "text", nullable: true),
+                    timestamp = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    sender_account_id = table.Column<int>(type: "integer", nullable: false),
+                    receiver_account_id = table.Column<int>(type: "integer", nullable: false),
+                    payment_intent_id = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("p_k_transactions", x => x.id);
+                    table.ForeignKey(
+                        name: "f_k_transactions_bank_accounts_receiver_account_id",
+                        column: x => x.receiver_account_id,
+                        principalTable: "bank_accounts",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "f_k_transactions_bank_accounts_sender_account_id",
+                        column: x => x.sender_account_id,
+                        principalTable: "bank_accounts",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "f_k_transactions_payment_intents_payment_intent_id",
+                        column: x => x.payment_intent_id,
+                        principalTable: "payment_intents",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "i_x_account_pins_bank_account_id",
+                table: "account_pins",
+                column: "bank_account_id");
+
+            migrationBuilder.CreateIndex(
+                name: "i_x_bank_accounts_bank_id",
+                table: "bank_accounts",
+                column: "bank_id");
+
+            migrationBuilder.CreateIndex(
+                name: "i_x_banks_name",
+                table: "banks",
+                column: "name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "i_x_idempotency_keys_response_body_id",
+                table: "idempotency_keys",
+                column: "response_body_id");
+
+            migrationBuilder.CreateIndex(
+                name: "i_x_payment_intents_receiver_account_id",
+                table: "payment_intents",
+                column: "receiver_account_id");
+
+            migrationBuilder.CreateIndex(
+                name: "i_x_transactions_payment_intent_id",
+                table: "transactions",
+                column: "payment_intent_id");
+
+            migrationBuilder.CreateIndex(
+                name: "i_x_transactions_receiver_account_id",
+                table: "transactions",
+                column: "receiver_account_id");
+
+            migrationBuilder.CreateIndex(
+                name: "i_x_transactions_sender_account_id",
+                table: "transactions",
+                column: "sender_account_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "account_pins");
+
+            migrationBuilder.DropTable(
+                name: "idempotency_keys");
+
+            migrationBuilder.DropTable(
+                name: "transactions");
+
+            migrationBuilder.DropTable(
+                name: "transaction_response");
+
+            migrationBuilder.DropTable(
+                name: "payment_intents");
+
+            migrationBuilder.DropTable(
+                name: "bank_accounts");
+
+            migrationBuilder.DropTable(
+                name: "banks");
+        }
+    }
+}
