0433b1f9035c6517ca1cfae3015aa783e09e7497
Eightball <eightball.code@gmail.com>
2026-09-04 10:47:07 +0545

    test: install XUnit and write a test for PinHasher
    - In the `Ntay.csproj`, add `Ntay.Tests` directory as exclusion so
      dotnet build ignores the test files. We do not want dotnet to build
      the test files which will fail the build.

---
+++ dotnet-backend/Ntay.Tests/Security/PinHasherTests.cs
@@ -0,0 +1,27 @@
+﻿using Ntay.Security;
+using Xunit;
+
+namespace Ntay.Tests.Security;
+
+public class PinHasherTests
+{
+    [Fact]
+    public void Verify_WithCorrectPin_ReturnsTrue()
+    {
+        string hashedPin = PinHasher.HashPin("1100");
+
+        bool result = PinHasher.VerifyPin("1100", hashedPin);
+
+        Assert.True(result);
+    }
+
+    [Fact]
+    public void Verify_WithWrongPin_ReturnsFalse()
+    {
+        string hashedPin = PinHasher.HashPin("1100");
+
+        bool result = PinHasher.VerifyPin("1234", hashedPin);
+
+        Assert.False(result);
+    }
+}
